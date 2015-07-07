316ebe973c8ac24e73f8bea6c6db37916c1c915d
makollers <markus.kollers@matrix42.com>
2015-07-07 13:17:34 +0200

    rollback to materializecss, basic implemetation of registration and login

--- Schmond/App/controllers/accountController.js
+++ Schmond/App/controllers/accountController.js
@@ -1,10 +1,10 @@
 ﻿'use strict';
-app.controller('accountController', ['$scope', '$rootScope', '$http', '$location', '$timeout', function ($scope, $rootScope, $http, $location, $timeout) {
+app.controller('accountController', ['$scope', '$rootScope', '$http', '$location', '$timeout', 'authService', function ($scope, $rootScope, $http, $location, $timeout, authService) {
 
 	var redirect = function (path) {
 		var timer = $timeout(function () {
 			$timeout.cancel(timer);
-			$rootScope.successMessage = '';
+			$rootScope.message = '';
 			$location.path(path);
 		}, 5000);
 	};
@@ -13,15 +13,38 @@ app.controller('accountController', ['$scope', '$rootScope', '$http', '$location
 		$rootScope.title = 'Registrieren';
 	}
 
+	$scope.initLogin = function () {
+		$rootScope.title = 'Anmelden';
+	}
+
 	$scope.register = function () {
 		$rootScope.loading = true;
 		$http.post('/api/accounts', $scope.user).success(function () {
-			$rootScope.successMessage = 'Registrierung erfolgreich abgeschlossen! In 5 Sekunden werden Sie zur Anmeldung weitergeleitet.';
+			$rootScope.state = 'success';
+			$rootScope.message = 'Registrierung erfolgreich abgeschlossen! In 5 Sekunden werden Sie zur Anmeldung weitergeleitet.';
 			redirect('/login');
 		}).error(function (err) {
-			console.log(err);
+			$rootScope.state = 'error';
 			$rootScope.message = err;
 		});
+
+		$rootScope.loading = false;
+	}
+
+	$scope.login = function () {
+		$rootScope.loading = true;
+
+		authService.login($scope.user).then(function () {
+			$rootScope.loading = false;
+			$rootScope.state = 'success';
+			$rootScope.message = 'Erfolgreich angemeldet! In 5 Sekunden werden Sie weitergeleitet.';
+			$location.path('/login');
+		},
+		 function (err) {
+		 	$rootScope.loading = false;
+		 	$rootScope.state = 'error';
+		 	$rootScope.message = err.error_description;
+		 });
 
 		$rootScope.loading = false;
 	}
--- Schmond/App/controllers/indexController.js
+++ Schmond/App/controllers/indexController.js
@@ -1,5 +1,8 @@
 ﻿'use strict';
-app.controller('indexController', ['$scope', '$rootScope', '$http', '$location', function ($scope, $rootScope, $http, $location) {
+app.controller('indexController', ['$scope', '$rootScope', '$http', '$location', 'authService', function ($scope, $rootScope, $http, $location, authService) {
+	$rootScope.message = '';
+	$scope.authentication = authService.authentication;
+
 	$scope.isActive = function (viewLocation) {
 		return viewLocation === $location.path();
 	};
