75ab4628915da81d5ac5ce61158d158379409f6a
makollers <markus.kollers@matrix42.com>
2015-07-13 15:38:10 +0200

    example for new entity in context and how you can load relationships

--- Schmond/Context.cs
+++ Schmond/Context.cs
@@ -3,20 +3,26 @@ using System.Data.Entity.ModelConfiguration.Conventions;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Schmond.Models;
 
-namespace Schmond {
-    public class Context : IdentityDbContext<User> {
-        public Context()
-            : base("DefaultContext") {
-            Database.SetInitializer<Context>(null);
-        }
+namespace Schmond
+{
+	public class Context : IdentityDbContext<User>
+	{
+		public Context()
+			: base("DefaultContext")
+		{
+			Database.SetInitializer<Context>(null);
+		}
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
 
-            modelBuilder.Entity<IdentityUserLogin>().HasKey(l => new { l.UserId, l.LoginProvider, l.ProviderKey });
-            modelBuilder.Entity<IdentityRole>().HasKey(r => r.Id);
-            modelBuilder.Entity<IdentityUserRole>().HasKey(r => new { r.RoleId, r.UserId });
-        }
-    }
+			modelBuilder.Entity<IdentityUserLogin>().HasKey(l => new { l.UserId, l.LoginProvider, l.ProviderKey });
+			modelBuilder.Entity<IdentityRole>().HasKey(r => r.Id);
+			modelBuilder.Entity<IdentityUserRole>().HasKey(r => new { r.RoleId, r.UserId });
+		}
+
+		public virtual IDbSet<Char> Chars { get; set; }
+	}
 }
---
+++ Schmond/Models/Char.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Schmond.Models
+{
+	[Table("Chars")]
+	public class Char
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+
+		[ForeignKey("UserId")]
+		public virtual User User { get; set; }
+
+		[Required]
+		public int UserId { get; set; }
+
+		//Example how to load relations
+		//using (Context ctx = new Context())
+		//{
+		//	var test = (from c in ctx.Chars.Include(x => x.User) // if you remove the include part, the user will be null. if you have the include part, ef will join the tables and load the whole user data, so be careful :)
+		//							select c);
+		//}
+	}
+}
