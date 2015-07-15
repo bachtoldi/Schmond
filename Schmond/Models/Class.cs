c5909734e7a7f975e3263aa4fe3be04ad69b5164
makollers <markus.kollers@matrix42.com>
2015-07-15 18:34:58 +0200

    created entity "class"

---
+++ Schmond/Models/Class.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Schmond.Models
+{
+	public class Class
+	{
+		[Key]
+		[Column("ClassId")]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+
+		[Required]
+		[Column("ClassName")]
+		public string Name { get; set; }
+	}
+}
