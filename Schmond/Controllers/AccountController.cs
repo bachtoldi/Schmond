4f0c70e33f6368a2ef6c5b02ec43e6a133b5567f
mkollers <mail@markuskollers.de>
2015-07-15 22:31:52 +0200

    edit account fix

--- Schmond/Controllers/AccountController.cs
+++ Schmond/Controllers/AccountController.cs
@@ -9,124 +9,127 @@ using System.Web.Http.ModelBinding;
 
 namespace Schmond.Controllers
 {
-    [RoutePrefix("api/accounts")]
-    public class AccountController : ApiController
-    {
-        private readonly AuthRepository _repo;
+	[RoutePrefix("api/accounts")]
+	public class AccountController : ApiController
+	{
+		private readonly AuthRepository _repo;
 
-        public AccountController()
-        {
-            _repo = new AuthRepository();
-        }
+		public AccountController()
+		{
+			_repo = new AuthRepository();
+		}
 
-        // POST api/accounts
-        [AllowAnonymous]
-        [HttpPost]
-        [Route("")]
-        public async Task<IHttpActionResult> Register(User userModel)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            var result = await _repo.RegisterUser(userModel);
+		// POST api/accounts
+		[AllowAnonymous]
+		[HttpPost]
+		[Route("")]
+		public async Task<IHttpActionResult> Register(User userModel)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+			var result = await _repo.RegisterUser(userModel);
 
-            var errorResult = GetErrorResult(result);
+			var errorResult = GetErrorResult(result);
 
-            if (errorResult != null)
-            {
-                return errorResult;
-            }
+			if (errorResult != null)
+			{
+				return errorResult;
+			}
 
-            return Ok();
-        }
+			return Ok();
+		}
 
-        [HttpGet]
-        [Route("")]
-        [Authorize]
-        public IHttpActionResult GetUserList()
-        {
-            var userList = _repo.GetUserList().Select(user => new
-            {
-                user.Id,
-                user.UserName,
-                user.Email,
-                MainCharId = user.FK_CharId
-            });
-            return Ok(userList);
-        }
+		[HttpGet]
+		[Route("")]
+		[Authorize]
+		public IHttpActionResult GetUserList()
+		{
+			var userList = _repo.GetUserList().Select(user => new
+			{
+				user.Id,
+				user.UserName,
+				user.Email,
+				user.MainCharId
+			});
+			return Ok(userList);
+		}
 
-        [HttpGet]
-        [Route("{id}")]
-        [Authorize]
-        public async Task<IHttpActionResult> GetUserById(string id)
-        {
-            var user = await _repo.GetUserById(id);
-            return Ok(new
-            {
-                user.Id,
-                user.Email,
-                user.UserName,
-                MainCharId = user.FK_CharId
-            });
-        }
+		[HttpGet]
+		[Route("{id}")]
+		[Authorize]
+		public async Task<IHttpActionResult> GetUserById(string id)
+		{
+			var user = await _repo.GetUserById(id);
+			return Ok(new
+			{
+				user.Id,
+				user.Email,
+				user.UserName,
+				user.MainCharId
+			});
+		}
 
-        [HttpPut]
-        [Route("{id}")]
-        [Authorize]
-        public async Task<IHttpActionResult> UpdateUser(string id, User userModel)
-        {
-            ModelState.Remove("userModel.ConfirmPassword");
-            ModelState.Remove("userModel.Password");
-            IHttpActionResult errorResult = GetErrorResult(await _repo.UpdateUser(id, userModel));
-            return (errorResult != null) ? errorResult : Ok();
-        }
+		[HttpPut]
+		[Route("{id}")]
+		[Authorize]
+		public async Task<IHttpActionResult> UpdateUser(string id, User userModel)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
 
-        //[HttpPut]
-        //[Route("password")]
-        //[Authorize]
-        //public async Task<IHttpActionResult> UpdatePassword(User userModel)
-        //{
+			var errorResult = GetErrorResult(await _repo.UpdateUser(id, userModel));
+			return (errorResult != null) ? errorResult : Ok();
+		}
 
-        //}
+		//[HttpPut]
+		//[Route("password")]
+		//[Authorize]
+		//public async Task<IHttpActionResult> UpdatePassword(User userModel)
+		//{
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                _repo.Dispose();
-            }
+		//}
 
-            base.Dispose(disposing);
-        }
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				_repo.Dispose();
+			}
 
-        private IHttpActionResult GetErrorResult(IdentityResult result)
-        {
-            if (result == null)
-            {
-                return InternalServerError();
-            }
+			base.Dispose(disposing);
+		}
 
-            if (result.Succeeded)
-            {
-                return null;
-            }
+		private IHttpActionResult GetErrorResult(IdentityResult result)
+		{
+			if (result == null)
+			{
+				return InternalServerError();
+			}
 
-            if (result.Errors != null)
-            {
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError("", error);
-                }
-            }
+			if (result.Succeeded)
+			{
+				return null;
+			}
 
-            if (ModelState.IsValid)
-            {
-                // No ModelState errors are available to send, so just return an empty BadRequest.
-                return BadRequest();
-            }
+			if (result.Errors != null)
+			{
+				foreach (var error in result.Errors)
+				{
+					ModelState.AddModelError("", error);
+				}
+			}
 
-            return BadRequest(ModelState);
-        }
-    }
+			if (ModelState.IsValid)
+			{
+				// No ModelState errors are available to send, so just return an empty BadRequest.
+				return BadRequest();
+			}
+
+			return BadRequest(ModelState);
+		}
+	}
 }
