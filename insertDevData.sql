d90191a7a6d234391e0165a1dcb96c74ba977fb9
makollers <markus.kollers@matrix42.com>
2015-07-16 15:43:09 +0200

    fixed dummy data

--- insertDevData.sql
+++ insertDevData.sql
@@ -1,5 +1,6 @@
-﻿INSERT INTO [Account] (Id,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName,FK_CharId)
-VALUES ('063b483e-6408-44e3-8fec-1466f48f43b9','Admin@Legion-Gilde.de',0,'AHoyu/CzirxOp4nmY51gkUdpzBOD4JQwn/0pfsrRmkeGqCNnUF5lmTTmsCQze3/eHQ==','516c4dd4-c938-483b-8b40-42d1ae74ea44',NULL,0,0,NULL,0,0,'Admin', NULL)
+﻿-- PW: Schmond
+INSERT INTO [Account] (Id,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName,FK_CharId)
+VALUES ('063b483e-6408-44e3-8fec-1466f48f43b9','Admin@Legion-Gilde.de',0,'AB/VNVgKGrlqcDjAbymWMnUp1E1D7aAyVJxEBGcnoMPKbu9HjLgnUBopcR8EbDyVQw==','516c4dd4-c938-483b-8b40-42d1ae74ea44',NULL,0,0,NULL,0,0,'Admin', NULL)
 
 INSERT INTO [Role] (Id, Name) VALUES ('DED7D181-7575-40F2-8734-4BC1FAB55131', 'Administrator')
 
